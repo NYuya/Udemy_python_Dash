95db688b498f368c2a08637d97435bb66bb18eca
NYuya <noooochan5@gmail.com>
2020-06-21 16:04:44 +0900

    3-13 Checkboxes,Input,Slider

--- sample01.py
+++ sample01.py
@@ -1,4 +1,3 @@
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -13,7 +12,7 @@ app.layout = html.Div([
         options=[
             {'label': '佐藤', 'value': 'sato'},
             {'label': '鈴木', 'value': 'suzuki'},
-            {'label': '田中', 'value': 'tanaka'}
+            {'label': '田中', 'value': 'tanaka'},
         ],
         value='suzuki'
     ),
@@ -23,7 +22,7 @@ app.layout = html.Div([
         options=[
             {'label': '佐藤', 'value': 'sato'},
             {'label': '鈴木', 'value': 'suzuki'},
-            {'label': '田中', 'value': 'tanaka'}
+            {'label': '田中', 'value': 'tanaka'},
         ],
         value=['sato', 'suzuki'],
         multi=True
@@ -34,13 +33,32 @@ app.layout = html.Div([
         options=[
             {'label': '佐藤', 'value': 'sato'},
             {'label': '鈴木', 'value': 'suzuki'},
-            {'label': '田中', 'value': 'tanaka'}
+            {'label': '田中', 'value': 'tanaka'},
         ],
         value='suzuki'
+    ),
+
+    html.Label('Checkboxes'),
+    dcc.Checklist(
+        options=[
+            {'label': '佐藤', 'value': 'sato'},
+            {'label': '鈴木', 'value': 'suzuki'},
+            {'label': '田中', 'value': 'tanaka'},
+        ],
+        value=['suzuki', 'tanaka']
+    ),
+
+    html.Label('Text Input'),
+    dcc.Input(value='佐藤', type='text'),
+
+    html.Label('Slider'),
+    dcc.Slider(
+        min=0,
+        max=5,
+        marks={i: i for i in range(1, 6)},
+        value=3
     )
-
-
-])
+], style={'columnCount': 2})
 
 if __name__ == '__main__':
     app.run_server(debug=True)
